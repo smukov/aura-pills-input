0e4825b6ff5ce037079640c362146c0d3dc080eb
Miroslav Smukov <smukov@gmail.com>
2019-03-22 22:14:16 +0100

    fixed indentation

--- mdapi/aura/demoPillsInput/demoPillsInputController.js
+++ mdapi/aura/demoPillsInput/demoPillsInputController.js
@@ -1,9 +1,9 @@
 ({
-	onPrintData : function(cmp, event, helper) {
-		const ipData = cmp.get("v.ipData");
+  onPrintData : function(cmp, event, helper) {
+    const ipData = cmp.get("v.ipData");
     const emailData = cmp.get("v.emailData");
     
     console.log('ip data: ', ipData);
     console.log('email data: ', emailData);
-	}
+  }
 })
